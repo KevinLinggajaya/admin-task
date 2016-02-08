4efd22fd295ca6b2c6a902ed485e43866358f3b2
MuzzaOpenCopy <muzza@Serenity.local>
2016-02-08 21:46:42 +0800

    Basic view
    Add basic view with bootstrap and some instructions

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,7 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.sass('app.scss')
+    .browserify('app.js')
+    .copy('node_modules/bootstrap-sass/assets/fonts', 'public/fonts');
 });
---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.master')
+
+@section('content')
+      <div class="starter-template">
+        <h1>Main Admin Page</h1>
+        <p class="lead">You can use this page as a basis for the planned tasks.<br> We are not evaluating your html/css skills - but are more focused on your use of the laravel framework.</p>
+      	<p>Please keep the html/css simple.</p>
+      	<p>Required functionality:
+      	    <ul>
+      	    	<li>Basic list of rows</li>
+      	    	<li>Add new row</li>
+      	    	<li>Edit row</li>
+      	    	<li>Delete row - with confirmation</li>
+            </ul>
+      	</p>
+      </div>
+@endsection
